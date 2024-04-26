4cafa59ac323ff5bbb23355e5a38bad27309a35b
Onipote <148326986+Onipote@users.noreply.github.com>
2024-04-26 10:05:53 +0200

    Suppression problème

---
+++ Assets/InaccessibleArea/InaccessibleAreaSystem.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InaccessibleAreaSystem : MonoBehaviour
+{
+
+    /* if (collision.gameObject.CompareTag("InaccessibleArea") /sans le collier)
+     {
+         Debug.Log("Vous ne passerez pas tant que vous en prouverez pas que vous être bien de la famille royale !");
+        //Joueur doit retourner au Hub (le château, zone 1) si le collier n'a pas été pris au début du jeu
+     }
+     //sinon
+     //Destroy(barrage.gameObject);*/
+}
