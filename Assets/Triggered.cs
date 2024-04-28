da20aa67a15fe53c0e2aa4f35e57db887d6b9053
Onipote <elsa.lefourn16@gmail.com>
2024-04-28 23:21:20 +0200

    Drop d'items par mort ennemi/obstacle + Rangement fichiers

--- Assets/Triggered.cs
+++ Assets/Triggered.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class Triggered : MonoBehaviour
 {
+    //Mort d'ennemi/obstacles => drop d'items
+    public GameObject lootMonster;
     public GameObject lootPrefab;
+    public GameObject loot2Prefab;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Monster"))
         {
+            Instantiate(lootMonster, collision.gameObject.transform.position, Quaternion.identity);
             Destroy(collision.gameObject);
         }
         else if (collision.CompareTag("Rock"))
         {
             Instantiate(lootPrefab, collision.gameObject.transform.position, Quaternion.identity);
+            Destroy(collision.gameObject);
+        }
+        else if (collision.CompareTag("TrainingDummy"))
+        {
+            Instantiate(lootPrefab, collision.gameObject.transform.position, Quaternion.identity);
+            Destroy(collision.gameObject);
+        }
+        else if (collision.CompareTag("SpecialTrainingDummy"))
+        {
+            Instantiate(loot2Prefab, collision.gameObject.transform.position, Quaternion.identity);
             Destroy(collision.gameObject);
         }
     }
